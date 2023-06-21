fea03564709084bab91430f441c0fb80a7248557
chrispatsalis <patsalis@umich.edu>
2023-06-21 18:43:57 -0400

    modified restructure_input_data and createDNEAobject functions so that input data must be un-normalized.

--- R/createDNEAobject.R
+++ R/createDNEAobject.R
@@ -1,10 +1,6 @@
 #'
 #'
 #'
-#'
-#'
-#'
-#'
 #'Structure input data for initialization of DNEAobject
 #'
 #'Manipulates input data and creates metadata for initialization of DNEAobject
@@ -12,7 +8,6 @@
 #' @param expression_data A matrix or dataframe of un-scaled expression data. The sample names should be rownames
 #'        and the feature names should be column names. Column 1 should be a factor of the two conditions, followed by
 #'        the numeric expression data
-#' @param scaled_expression_data A matrix or dataframe similar to expression_data but for scaled data
 #' @param control A string corresponding to the name of condition 1 in column one of the data matrix
 #' @param case A string corresponding to the name of condition 2 in column one of the data matrix
 #'
@@ -23,7 +18,7 @@
 #' @import methods
 #' @importFrom janitor make_clean_names
 #' @keywords internal
-restructure_input_data <- function(expression_data, scaled_expression_data, control, case){
+restructure_input_data <- function(expression_data, control, case){
 
   ######################
   #**INITIALIZE LISTS**#
@@ -44,78 +39,50 @@ restructure_input_data <- function(expression_data, scaled_expression_data, cont
   #**CHECK FOR PROPER CONDITIONS AND CONVERT DATA TO MATRIX**#
   ############################################################
 
-  #un-scaled data
-  if(missing(expression_data) == FALSE){
+  #check proper data structure
+  if(!is.character(expression_data[,1])) stop('First column should be sample condition')
 
-    #check proper data structure
-    if(is.numeric(expression_data[,1])) stop('First column should be sample condition')
+  #turn condition into factor
+  if(!(is.factor(expression_data[,1]))){
 
-    #turn condition into factor
-    if(!(is.factor(expression_data[,1]))){
-      expression_data[,1]<- factor(expression_data[,1], levels = c(control, case))
-      message(paste0('Condition for expression_data should be of class factor. Converting Now. \n',
-                     'Condition is now a factor with levels:',
-                     '\n', '1. ',
-                     levels(expression_data[,1])[1],
-                     '\n', '2. ',
-                     levels(expression_data[,1])[2]))
-    }
-
-    #clean column names and convert data to matrix
-    unscaled_feature_names <- colnames(expression_data[,-1])
-    unscaled_sample_names <- rownames(expression_data)
-    unscaled_condition_values <- expression_data[,1]
-    colnames(expression_data) <- make_clean_names(colnames(expression_data))
-    expression_data <- as.matrix(expression_data[,-1])
-
-    #add to assays list
-    assays[['expression_data']] <- expression_data
+    expression_data[,1]<- factor(expression_data[,1], levels = c(control, case))
+    message(paste0('Condition for expression_data should be of class factor. Converting Now. \n',
+                   'Condition is now a factor with levels:',
+                   '\n', '1. ',
+                   levels(expression_data[,1])[1],
+                   '\n', '2. ',
+                   levels(expression_data[,1])[2]))
   }
 
-  #scaled data
-  if(missing(scaled_expression_data) == FALSE){
+  #grab relevant metadata
+  feature_names <- colnames(expression_data[,-1])
+  clean_feature_names <- make_clean_names(feature_names)
+  sample_names <- rownames(expression_data)
+  condition_values <- expression_data[,1]
 
-    #check proper data structure
-    if(is.numeric(scaled_expression_data[,1])) stop('First column should be sample condition')
+  #convert expression data to matrix
+  expression_data <- as.matrix(expression_data[,-1])
 
-    #turn condition into a factor
-    if(!(is.factor(scaled_expression_data[,1]))){
-      scaled_expression_data[,1]<- factor(scaled_expression_data[,1],levels = c(control, case))
-      cat(paste0('Condition for scaled_expression_data should be of class factor. Converting Now. \n',
-                 'Condition is now a factor with levels:',
-                 '\n', '1. ',
-                 levels(scaled_expression_data[,1])[1],
-                 '\n', '2. ',
-                 levels(scaled_expression_data[,1])[2],'\n\n'))
-    }
+  #clean column names to avoid R conflicts
+  colnames(expression_data) <- clean_feature_names
 
-    #clean column names and convert data to matrix
-    scaled_feature_names <- colnames(scaled_expression_data[,-1])
-    scaled_sample_names <- rownames(scaled_expression_data)
-    scaled_condition_values <- scaled_expression_data[,1]
-    colnames(scaled_expression_data) <- make_clean_names(colnames(scaled_expression_data))
-    scaled_expression_data <- as.matrix(scaled_expression_data[,-1])
-  } else {
+  #add to assays list
+  assays[['expression_data']] <- expression_data
 
-    #if no scaled data is provided, split by condition and scale the data
-    scaled_expression_data <- lapply(split_by_condition(dat = expression_data,
-                                                              condition_levels = levels(unscaled_condition_values),
-                                                              condition_by_sample = unscaled_condition_values),
-                                           function(x) scale(t(x)))
+  ##scale the expression data for analysis
+  #split by group
+  scaled_expression_data <- lapply(split_by_condition(dat = expression_data,
+                                                      condition_levels = levels(condition_values),
+                                                      condition_by_sample = condition_values),
+                                   function(x) scale(t(x)))
 
-    #combine scaled data into one matrix
-    scaled_expression_data <- rbind(scaled_expression_data[[1]],scaled_expression_data[[2]])
+  #combine scaled data into one matrix
+  scaled_expression_data <- rbind(scaled_expression_data[[1]], scaled_expression_data[[2]])
 
-    #order samples to be same as un-scaled
-    scaled_expression_data <- scaled_expression_data[rownames(expression_data),]
+  #order samples to be same as un-scaled
+  scaled_expression_data <- scaled_expression_data[rownames(expression_data), ]
 
-    message('Data has been normalized for further analysis. New data can be found in the scaled_expression_data assay!\n')
-
-    #set the metadata equal to un-scaled counterpart
-    scaled_feature_names <- unscaled_feature_names
-    scaled_sample_names <- unscaled_sample_names
-    scaled_condition_values <- unscaled_condition_values
-  }
+  message('Data has been normalized for further analysis. New data can be found in the scaled_expression_data assay!\n')
 
   ##############################################
   #**add data to initialized lists for output**#
@@ -125,12 +92,12 @@ restructure_input_data <- function(expression_data, scaled_expression_data, cont
   assays[['scaled_expression_data']] <- scaled_expression_data
 
   #Add features, samples, condition to metadata
-  metadata[["samples"]] <- data.frame(samples = scaled_sample_names,
-                                      conditions = scaled_condition_values,
-                                      row.names = scaled_sample_names)
-  metadata[["features"]] <- data.frame(feature_names = scaled_feature_names,
-                                       clean_feature_names = make_clean_names(scaled_feature_names),
-                                       row.names = scaled_feature_names)
+  metadata[["samples"]] <- data.frame(samples = sample_names,
+                                      conditions = condition_values,
+                                      row.names = sample_names)
+  metadata[["features"]] <- data.frame(feature_names = feature_names,
+                                       clean_feature_names = clean_feature_names,
+                                       row.names = feature_names)
 
   return(list(assays, metadata))
 
@@ -156,47 +123,20 @@ restructure_input_data <- function(expression_data, scaled_expression_data, cont
 #' feature number, sample names, feature names, and condition number in metadata
 #'
 #' @export
-createDNEAobject <- function(project_name, expression_data, scaled_expression_data, control, case){
+createDNEAobject <- function(project_name, expression_data, control, case){
 
-  ##check data input to make sure it matches
-  if(missing(expression_data) == FALSE & missing(scaled_expression_data) == FALSE){
-    if(all(colnames(expression_data[,-1]) != colnames(scaled_expression_data[,-1])) |
-       all(rownames(expression_data[,-1]) != rownames(scaled_expression_data[,-1])) |
-       all(expression_data[,1] != scaled_expression_data[,1])) stop("Data matrices must have identical features, samples, and condition values")
-  }
 
   ##restructure data to initiate object
-  if(missing(scaled_expression_data) == FALSE & missing(expression_data) == FALSE){
+  if(!missing(expression_data)){
 
-    #create data structures to initialize DNEAobject with scaled and un-scaled data
+    #create data structures to initialize DNEAobject with un-scaled data
     restructured_data <- restructure_input_data(expression_data = expression_data,
-                                                scaled_expression_data = scaled_expression_data,
                                                 control = control,
                                                 case = case)
   } else{
 
-    if(missing(expression_data) == FALSE){
-
-      #create data structures to initialize DNEAobject with un-scaled data
-      restructured_data <- restructure_input_data(expression_data = expression_data,
-                                                  control = control,
-                                                  case = case)
-    } else {
-
-      if(missing(scaled_expression_data) == FALSE){
-
-        #create data structures to initialize DNEAobject with scaled data
-        restructured_data <- restructure_input_data(scaled_expression_data = scaled_expression_data,
-                                                    control = control,
-                                                    case = case)
-
-      } else{
-
-        #no data was provided - throw error
-        stop('Expression data must be provided to create DNEAobject')
-      }
-    }
-
+    #no data was provided - throw error
+    stop('Expression data must be provided to create DNEAobject')
   }
 
   ##initiate DNEA object
