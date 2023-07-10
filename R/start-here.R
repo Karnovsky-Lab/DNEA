ccbd3932f012c733c463698858e11b4cbe5e2d7f
chrispatsalis <patsalis@umich.edu>
2023-07-09 21:39:57 -0400

    created start-here file to be data and package documentation plus legend for package R code

---
+++ R/start-here.R
@@ -0,0 +1,15 @@
+
+
+
+#' "TEDDY" example data from xxx
+#'
+#' This data is an \emph{n x m} expression matrix corresponding to a curated list of metabolites from "The Environmental Determinants of
+#' Diabetes in the Young" clinical trial.
+#'
+#' @format ## `TEDDY`
+#' A data frame with 100 rows and 144 columns. Each row corresponds to a sample, and each column corresponds to a metabolite.
+#'
+#' @references Lee HS, Burkhardt BR, McLeod W, Smith S, Eberhard C, Lynch K, Hadley D, Rewers M, Simell O, She JX, Hagopian B, Lernmark A, Akolkar B, Ziegler AG, Krischer JP; TEDDY study group. Biomarker discovery study design for type 1 diabetes in The Environmental Determinants of Diabetes in the Young (TEDDY) study. Diabetes Metab Res Rev. 2014 Jul;30(5):424-34. doi: 10.1002/dmrr.2510. PMID: 24339168; PMCID: PMC4058423. \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4058423/}
+#' @source The raw data can be downloaded from the Metabolomics workbench under study ID \strong{ST001386}:
+#' \url{https://www.metabolomicsworkbench.org/data/DRCCStudySummary.php?Mode=SetupRawDataDownload&StudyID=ST001386}
+"TEDDY"
