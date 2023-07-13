1831eb187f72c7ad5df610ad02f334ce0610df6c
chrispatsalis <patsalis@umich.edu>
2023-07-12 20:44:51 -0400

    updated man pages and removed joint_graph slot

--- R/all-classes.R
+++ R/all-classes.R
@@ -136,7 +136,7 @@ setValidity("consensusClusteringResults", function(object){
     "there was a problem with consensus cluster results object"
   }
   for(i in length(object@adjacency_graphs)){
-    if(class(object@adjacency_graphs[i]) != "igraph"){
+    if(inherits(object@adjacency_graphs[i], what = "igraph")){
       "There was a problem with adjacency graphs in consensus clustering"
     }
   }
@@ -300,7 +300,7 @@ setValidity("DNEAresults", function(object){
   }
 
   #check consensus_clustering
-  if(all(CCsummary(object) == c(0, 0))){
+  if(all(CCsummary(object) != c(0, 0))){
 
     if(sum(object@consensus_clustering@summary$number_of_nodes) != ncol(expressionData(object, normalized = FALSE))){
       "There was a problem with consensus clustering"
