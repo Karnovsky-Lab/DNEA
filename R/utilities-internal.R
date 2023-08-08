eb37d6e14a6c34fd4ccebe7d1421d7f33ac1e870
chrispatsalis <patsalis@umich.edu>
2023-08-08 16:05:23 -0400

    added check to throw error if condition length does not match number of columns in matrix

--- R/utilities-internal.R
+++ R/utilities-internal.R
@@ -3,7 +3,7 @@
 #' split_by_condition will separate the input expression matrix into a list of matrices. Each matrix
 #' corresponds to the expression data for one condition specified by condition_levels
 #'
-#' @param dat a matrix of expression data wherein the samples are rows and features are columns.
+#' @param dat a matrix of expression data wherein the features are rows and samples are columns
 #' @param condition_levels A list or vector of the unique conditions present in dat
 #' @param condition_by_sample A list or vector of the condition value corresponding to each
 #'        sample.
@@ -11,6 +11,9 @@
 #' @keywords internal
 #' @noRd
 split_by_condition <- function(dat, condition_levels, condition_by_sample){
+
+  #check input
+  if(ncol(dat) != length(condition_by_sample)) stop("The provided conditions do not correspond to the samples in expression matrix!")
 
   #create key for separating the data by key and running diagnostic tests, feature DE calculations
   separated_conditions_data <- vector(mode = 'list', length = length(condition_levels))
