fb3f1a936a4c47e08d889140f01ab479e6035356
chrispatsalis <patsalis@umich.edu>
2023-08-16 12:33:39 -0400

    fixed error in format section of data man pages

--- R/start-here.R
+++ R/start-here.R
@@ -1,19 +1,19 @@
 #################################################################################################################################################
-#* DNEA package organization *#                                                                                                                 #
+#* DNEAdev package organization *#                                                                                                                 #
 ###############################                                                                                                                 #
 #                                                                                                                                               #
 # start-here.R.....................This file - contains the package legend as well as package and data documentation                            #
-# createDNEAobject.R...............contains the exported createDNEAobject() function that initiates the DNEA workflow. The internal helper      #
+# createDNEAobject.R...............contains the exported createDNEAobject() function that initiates the DNEAdev workflow. The internal helper      #
 #                                  functions called by createDNEAobject (ie. restructure_input_data(), dataDiagnostics(), metabDE()) are also   #
 #                                  located in this file                                                                                         #
 # collapseNodes-fun.R..............contains the exported wrapper function(reduceFeatures()) for node collapsing as well as the internal         #
 #                                  helper functions called by said function (ie. collapseNodes_cor(), collapseNodes_knowledge(),                #
 #                                  and collapseNodes_hybrid())                                                                                  #
-# MainFunctions.R..................contains the exported wrapper functions for each step of DNEA (ie. BICtune(), stabilitySelection(),          #
+# MainFunctions.R..................contains the exported wrapper functions for each step of DNEAdev (ie. BICtune(), stabilitySelection(),          #
 #                                  getNetworks(), clusterNet(), runNetGSA())                                                                    #
 # utilities-internal.R.............contains internal utility functions used by exported functions (ie. split_by_condition)                      #
 # utilities-external.R.............contains exported utility functions that adds functionality for the user but not part of                     #
-#                                  the DNEA workflow (ie. includeMetadata(), plotNetworks(), getNetworkFiles(), filterNetworks())               #
+#                                  the DNEAdev workflow (ie. includeMetadata(), plotNetworks(), getNetworkFiles(), filterNetworks())               #
 # all-classes.R....................contains the custom s4 classes and validator functions                                                       #
 # all-generics.R...................contains all s4 generics                                                                                     #
 # all-methods.R....................contains all s4 custom methods                                                                               #
@@ -32,7 +32,7 @@
 #' @keywords datasets
 #' @name TEDDY
 #' @usage data("TEDDY")
-#' @format ## `TEDDY` A data frame with 100 rows and 144 columns. Each row corresponds to a sample, and each column
+#' @format A data frame with 100 rows and 144 columns. Each row corresponds to a sample, and each column
 #'         corresponds to a metabolite.
 #' @references Lee HS, Burkhardt BR, McLeod W, Smith S, Eberhard C, Lynch K, Hadley D, Rewers M, Simell O, She JX, Hagopian B, Lernmark A, Akolkar B, Ziegler AG, Krischer JP; TEDDY study group. Biomarker discovery study design for type 1 diabetes in The Environmental Determinants of Diabetes in the Young (TEDDY) study. Diabetes Metab Res Rev. 2014 Jul;30(5):424-34. doi: 10.1002/dmrr.2510. PMID: 24339168; PMCID: PMC4058423. \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4058423/}
 #' @source The raw data can be downloaded from the Metabolomics workbench under study ID \strong{ST001386}:
@@ -49,7 +49,7 @@
 #' @keywords datasets
 #' @name T1Dmeta
 #' @usage data("T1Dmeta")
-#' @format ## `T1Dmeta` A data frame with 322 rows and 9 columns. Each row corresponds to a sample, and each column corresponds to:
+#' @format A data frame with 322 rows and 9 columns. Each row corresponds to a sample, and each column corresponds to:
 #' \describe{
 #'   \item{subject}{The individual patient}
 #'   \item{Endpoint1}{The age of the case subject in days when this sample was collected}
@@ -61,30 +61,28 @@
 #'   \item{sample}{The name of this sample}
 #'   \item{group}{A variable indicating whether or not this sample is part of the T1D case or T1D control group}
 #' }
-#'
 #' @references Lee HS, Burkhardt BR, McLeod W, Smith S, Eberhard C, Lynch K, Hadley D, Rewers M, Simell O, She JX, Hagopian B, Lernmark A, Akolkar B, Ziegler AG, Krischer JP; TEDDY study group. Biomarker discovery study design for type 1 diabetes in The Environmental Determinants of Diabetes in the Young (TEDDY) study. Diabetes Metab Res Rev. 2014 Jul;30(5):424-34. doi: 10.1002/dmrr.2510. PMID: 24339168; PMCID: PMC4058423. \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4058423/}
 #' @source The raw data can be downloaded from the Metabolomics workbench under study ID \strong{ST001386}:
 #' \url{https://www.metabolomicsworkbench.org/data/DRCCStudySummary.php?Mode=SetupRawDataDownload&StudyID=ST001386}
 "T1Dmeta"
 
-#' Example results for DNEA
+#' Example results for DNEAdev
 #'
-#' This data is an example DNEAresults object after performing DNEA. The experiment was performed using the \code{\link{TEDDY}}
+#' This data is an example DNEAresults object after performing DNEAdev. The experiment was performed using the \code{\link{TEDDY}}
 #' data with 4 reps of stability selection, no subsampling, and default parameters everywhere else. The seed was set to 417.
 #'
-#' @returns A \code{\link{DNEAresults}} object containing the results of a DNEA experiment
+#' @returns A \code{DNEAresults} object containing the results of a DNEAdev experiment
 #' @docType data
 #' @keywords datasets
 #' @name TEDDYresults
 #' @usage data("TEDDYresults")
-#' @format ## `TEDDYresults` A DNEA results object after completing a DNEA experiment.
-#'
-#' @source The data is stored in the \code{\link{DNEA}} package and can be accessed by running data(TEDDYresults) in the console.
+#' @format A DNEAdev results object after completing a DNEAdev experiment.
+#' @source The data is stored in the \code{\link{DNEAdev}} package and can be accessed by running data(TEDDYresults) in the console.
 "TEDDYresults"
 
-#' DNEA package for joint estimation of biological networks
+#' DNEAdev package for joint estimation of biological networks
 #'
-#' DNEA is designed to take as input non-normalized, non-transformed -omics data
+#' DNEAdev is designed to take as input non-normalized, non-transformed -omics data
 #' (ie. metabolomics, lipidomics, proteomics) and normalize the expression values, optimize the regularization
 #' parameter, and jointly estimate the biological networks across a user-specified experimental
 #' condition. The resulting networks may then be clustered to identify metabolic modules,
@@ -100,12 +98,12 @@
 #' \item \strong{\code{\link{clusterNet}}}
 #' \item \strong{\code{\link{runNetGSA}}}}
 #'
-#' A more descriptive workflow can be viewed in the package vignette. This can be accessed by running \code{vignette("DNEA")} in the console. \cr
+#' A more descriptive workflow can be viewed in the package vignette. This can be accessed by running \code{vignette("DNEAdev")} in the console. \cr
 #'
-#' The source code is available at the Karnovsky lab Github page: \url{https://github.com/Karnovsky-Lab/DNEA}
+#' The source code is available at the Karnovsky lab Github page: \url{https://github.com/Karnovsky-Lab/DNEAdev}
 #'
 #' @docType package
-#' @name DNEA
-#' @aliases DNEA
+#' @name DNEAdev
+#' @aliases DNEAdev
 #' @keywords package
 NULL
