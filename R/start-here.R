2f30c430e034acd92fc2ec6a27ddcb5fa75754cc
chrispatsalis <patsalis@umich.edu>
2023-08-23 17:40:37 -0400

    updated TEDDY references

--- R/start-here.R
+++ R/start-here.R
@@ -35,8 +35,10 @@
 #' @format A data frame with 100 rows and 144 columns. Each row corresponds to a sample, and each column
 #'         corresponds to a metabolite.
 #' @references Lee HS, Burkhardt BR, McLeod W, Smith S, Eberhard C, Lynch K, Hadley D, Rewers M, Simell O, She JX, Hagopian B, Lernmark A, Akolkar B, Ziegler AG, Krischer JP; TEDDY study group. Biomarker discovery study design for type 1 diabetes in The Environmental Determinants of Diabetes in the Young (TEDDY) study. Diabetes Metab Res Rev. 2014 Jul;30(5):424-34. doi: 10.1002/dmrr.2510. PMID: 24339168; PMCID: PMC4058423. \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4058423/}
-#' @source The raw data can be downloaded from the Metabolomics workbench under study ID \strong{ST001386}:
-#' \url{https://www.metabolomicsworkbench.org/data/DRCCStudySummary.php?Mode=SetupRawDataDownload&StudyID=ST001386}
+#' @source This data is available at the NIH Common Fund's National Metabolomics Data Repository (NMDR) website,
+#'         the Metabolomics Workbench, \url{https://www.metabolomicsworkbench.org}, where it has been assigned
+#'         Project ID PR000950 and Study ID ST001386. The data can be accessed directly via it's
+#'         Project DOI: \url{10.21228/M8WM4P}. This work is supported by NIH grant, U2C- DK119886.
 "TEDDY"
 
 #' The meta data file for the TEDDY metabolomics data
