f5db09f8a148acec06e011352e710b36c1035deb
chrispatsalis <patsalis@umich.edu>
2023-12-04 13:53:30 -0500

    updated data documentation

--- R/start-here.R
+++ R/start-here.R
@@ -1,94 +1,43 @@
-#################################################################################################################################################
-#* DNEA package organization *#                                                                                                                 #
-###############################
 ################################################################################
-#                                                                                                                                               #
-# start-here.R.....................This file - contains the package legend as well as package and data documentation                            #
-# initiator.R......................contains the exported createDNEAobject() function that initiates the DNEA workflow. The internal helper      #
-#                                  functions called by createDNEAobject (ie. restructure_input_data(), dataDiagnostics(), metabDE()) are also   #
-#                                  located in this file                                                                                         #
-# reduce-features.R................contains the exported wrapper function(aggregateFeatures()) for node collapsing as well as the internal         #
-#                                  helper functions called by said function (ie. collapseNodes_cor(), collapseNodes_knowledge(),                #
-#                                  and collapseNodes_hybrid())                                                                                  #
-# primary.R........................contains the exported wrapper functions for each step of DNEA (ie. BICtune(), stabilitySelection(),          #
-#                                  getNetworks(), clusterNet(), runNetGSA())                                                                    #
-# utilities-internals.R............contains internal utility functions used by exported functions (ie. split_by_condition)                      #
-# utilities-exported.R.............contains exported utility functions that adds functionality for the user but not part of                     #
-#                                  the DNEA workflow (ie. includeMetadata(), plotNetworks(), getNetworkFiles(), filterNetworks())               #
-# all-classes.R....................contains the custom s4 classes and validator functions                                                       #
-# all-generics.R...................contains all s4 generics                                                                                     #
-# all-methods.R....................contains all s4 custom methods                                                                               #
-# JSEM-internals.R.................contains all of the internal functions for joint estimation of biological networks (ie. functions            #
-#                                  called by BICtune(), stabilitySelection(), and getNetworks())                                                #
-# clustering-internals.R...........contains the internal functions necessary for consensus clustering                                           #
-#################################################################################################################################################
-
-#' The example data from xxx
-#'
-#' This data is an \emph{m x n} expression matrix corresponding to a curated list of metabolites from "The Environmental Determinants of
-#' Diabetes in the Young" clinical trial.
-#'
-#' @returns An \emph{m x n} expression matrix of metabolomics data from the TEDDY dataset
-#' @docType data
-#' @keywords datasets
-#' @name TEDDY
-#' @usage data("TEDDY")
-#' @format A data frame with 100 rows and 144 columns. Each row corresponds to a sample, and each column
-#'         corresponds to a metabolite.
-#' @references Lee HS, Burkhardt BR, McLeod W, Smith S, Eberhard C, Lynch K, Hadley D, Rewers M, Simell O, She JX, Hagopian B, Lernmark A, Akolkar B, Ziegler AG, Krischer JP; TEDDY study group. Biomarker discovery study design for type 1 diabetes in The Environmental Determinants of Diabetes in the Young (TEDDY) study. Diabetes Metab Res Rev. 2014 Jul;30(5):424-34. doi: 10.1002/dmrr.2510. PMID: 24339168; PMCID: PMC4058423. \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4058423/}
-#' @source This data is available at the NIH Common Fund's National Metabolomics Data Repository (NMDR) website,
-#'         the Metabolomics Workbench, \url{https://www.metabolomicsworkbench.org}, where it has been assigned
-#'         Project ID PR000950 and Study ID ST001386. The data can be accessed directly via it's
-#'         Project DOI: \url{10.21228/M8WM4P}. This work is supported by NIH grant, U2C- DK119886.
-"TEDDY"
-
-#' The meta data file for the TEDDY metabolomics data
-#'
-#' This data is a data frame containing metadata for the samples in the corresponding \code{\link{TEDDY}} example data from "The Environmental Determinants of
-#' Diabetes in the Young" clinical trial.
-#'
-#' @returns A data frame containing the metadata for the TEDDY metabolomics study
-#' @docType data
-#' @keywords datasets
-#' @name T1Dmeta
-#' @usage data("T1Dmeta")
-#' @format A data frame with 322 rows and 9 columns. Each row corresponds to a sample, and each column corresponds to:
-#' \describe{
-#'   \item{subject}{The individual patient}
-#'   \item{Endpoint1}{The age of the case subject in days when this sample was collected}
-#'   \item{Endpoint2}{The age of the control subject in days when this sample was collected}
-#'   \item{Age}{The age of the subject in days when this sample was collected}
-#'   \item{Sex}{The sex of the subject}
-#'   \item{sample}{The name of this sample}
-#'   \item{group}{A variable indicating whether or not this sample is part of the T1D case or T1D control group}
-#' }
-#' @references Lee HS, Burkhardt BR, McLeod W, Smith S, Eberhard C, Lynch K, Hadley D, Rewers M, Simell O, She JX, Hagopian B, Lernmark A, Akolkar B, Ziegler AG, Krischer JP; TEDDY study group. Biomarker discovery study design for type 1 diabetes in The Environmental Determinants of Diabetes in the Young (TEDDY) study. Diabetes Metab Res Rev. 2014 Jul;30(5):424-34. doi: 10.1002/dmrr.2510. PMID: 24339168; PMCID: PMC4058423. \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4058423/}
-#' @source The raw data can be downloaded from the Metabolomics workbench under study ID \strong{ST001386}:
-#' \url{https://www.metabolomicsworkbench.org/data/DRCCStudySummary.php?Mode=SetupRawDataDownload&StudyID=ST001386}
-"T1Dmeta"
-
-#' Example results for DNEA
-#'
-#' This data is an example DNEAobj object after performing DNEA. The experiment was performed using the \code{\link{TEDDY}}
-#' data with 4 reps of stability selection, no subsampling, and default parameters everywhere else. The seed was set to 417.
-#'
-#' @returns A \code{DNEAobj} object containing the results of a DNEA experiment
-#' @docType data
-#' @keywords datasets
-#' @name dnw
-#' @usage data("dnw")
-#' @format A DNEA results object after completing a DNEA experiment.
-#' @source The data is stored in the \code{\link{DNEA}} package and can be accessed by running data(dnw) in the console.
-"dnw"
-
-#' DNEA package for joint estimation of biological networks
-#'
-#' DNEA is designed to take as input non-normalized, non-transformed -omics data
-#' (ie. metabolomics, lipidomics, proteomics) and normalize the expression values, optimize the regularization
-#' parameter, and jointly estimate the biological networks across a user-specified experimental
-#' condition. The resulting networks may then be clustered to identify metabolic modules,
-#' aka subnetworks, within the larger networks. These subnetworks are then tested for enrichment across the experimental
-#' condition using the \code{\link[netgsa:NetGSA]{netgsa::NetGSA()}} algorithm.
+#* DNEA package organization *#                                                #                                                                #
+###############################                                                #                                                                                         #
+# start-here.R.....................This file - contains the package legend as  #
+#                                  well as package and data documentation      #
+# initiator.R......................contains the exported createDNEAobject()    #
+#                                  function that initiates the DNEA workflow.  #
+#                                  The internal helper functions called by     #
+#                                  createDNEAobject (ie.                       #
+#                                  restructure_input_data(), dataDiagnostics(),#
+#                                  metabDE()) are also located in this file    #                                                                                    #
+# reduce-features.R................contains the exported wrapper function      #
+#                                  aggregateFeatures() for node aggregating as #
+#                                  well as the internal helper functions called#
+#                                  by said function (ie. collapseNodes_cor(),  #
+#                                  collapseNodes_knowledge(), and              #
+#                                  collapseNodes_hybrid())                     #
+# primary.R........................contains the exported wrapper functions for #
+#                                  each step of DNEA (ie. BICtune(),           #
+#                                  stabilitySelection(), getNetworks(),        #
+#                                  clusterNet(), runNetGSA())                  #
+# utilities-internals.R............contains internal utility functions used by #
+#                                  exported functions (ie. split_by_condition) #
+# utilities-exported.R.............contains exported utility functions that    #
+#                                  adds functionality for the user but not part#
+#                                  of the DNEA workflow (ie. includeMetadata(),#
+#                                  plotNetworks(), getNetworkFiles(),          #
+#                                  filterNetworks())                           #
+# all-classes.R....................contains the custom s4 classes and validator#
+#                                  functions                                   #
+# all-generics.R...................contains all s4 generics                    #                                                                   #
+# all-methods.R....................contains all s4 custom methods              #                                                                #
+# JSEM-internals.R.................contains all of the internal functions for  #
+#                                  joint estimation of biological networks (ie.#
+#                                  functions called by BICtune(),              #
+#                                  stabilitySelection(), and getNetworks())    #
+# clustering-internals.R...........contains the internal functions necessary   #
+#                                  for consensus clustering                    #
+################################################################################
+#' @section Primary Components:
 #'
 #' The main workflow contains the following functions:
 #' \enumerate{
@@ -99,12 +48,102 @@
 #' \item \strong{\code{\link{clusterNet}}}
 #' \item \strong{\code{\link{runNetGSA}}}}
 #'
-#' A more descriptive workflow can be viewed in the package vignette. This can be accessed by running \code{vignette("DNEA")} in the console. \cr
+#' A more descriptive workflow can be viewed in the package vignette.
+#' This can be accessed by running \code{vignette("DNEA")} in the console.
 #'
-#' The source code is available at the Karnovsky lab Github page: \url{https://github.com/Karnovsky-Lab/DNEA}
-#'
-#' @docType package
-#' @name DNEA
-#' @aliases DNEA
-#' @keywords package
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
+
+
+#' The example data from xxx
+#'
+#' This data is an \emph{m x n} expression matrix corresponding to a
+#' curated list of metabolites from "The Environmental Determinants of
+#' Diabetes in the Young" clinical trial.
+#'
+#' @returns An \emph{m x n} expression matrix of metabolomics data from the
+#' TEDDY dataset
+#' @docType data
+#' @keywords datasets
+#' @name TEDDY
+#' @usage data("TEDDY")
+#' @format A data frame with 100 rows and 144 columns. Each row corresponds
+#' to a sample, and each column corresponds to a metabolite.
+#' @references Lee HS, Burkhardt BR, McLeod W, Smith S, Eberhard C, Lynch K,
+#' Hadley D, Rewers M, Simell O, She JX, Hagopian B, Lernmark A, Akolkar B,
+#' Ziegler AG, Krischer JP; TEDDY study group. Biomarker discovery study
+#' design for type 1 diabetes in The Environmental Determinants of Diabetes
+#' in the Young (TEDDY) study. Diabetes Metab Res Rev. 2014 Jul;30(5):424-34.
+#' doi: 10.1002/dmrr.2510. PMID: 24339168; PMCID: PMC4058423.
+#' \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4058423/}
+#' @source This data is available at the NIH Common Fund's National
+#' Metabolomics Data Repository (NMDR) website, the Metabolomics Workbench,
+#' \url{https://www.metabolomicsworkbench.org}, where it has been assigned
+#' Project ID PR000950 and Study ID ST001386. The data can be accessed
+#' directly via it's Project DOI: \url{10.21228/M8WM4P}. This work is
+#' supported by NIH grant, U2C- DK119886.
+"TEDDY"
+
+#' The meta data file for the TEDDY metabolomics data
+#'
+#' This data is a data frame containing metadata for the samples
+#' in the corresponding \code{\link{TEDDY}} example data from
+#' "The Environmental Determinants of Diabetes in the Young" clinical trial.
+#'
+#' @returns A data frame containing the metadata for the TEDDY
+#' metabolomics study
+#' @docType data
+#' @keywords datasets
+#' @name T1Dmeta
+#' @usage data("T1Dmeta")
+#' @format A data frame with 322 rows and 9 columns. Each row corresponds
+#' to a sample, and each column corresponds to:
+#' \describe{
+#' \item{subject}{The individual patient}
+#' \item{Endpoint1}{The age of the case subject in days when this
+#' sample was collected}
+#' \item{Endpoint2}{The age of the control subject in days when
+#' this sample was collected}
+#' \item{Age}{The age of the subject in days when this sample
+#' was collected}
+#' \item{Sex}{The sex of the subject}
+#' \item{sample}{The name of this sample}
+#' \item{group}{A variable indicating whether or not this sample
+#' is part of the T1D case or T1D control group}}
+#'
+#' @references Lee HS, Burkhardt BR, McLeod W, Smith S, Eberhard C,
+#' Lynch K, Hadley D, Rewers M, Simell O, She JX, Hagopian B,
+#' Lernmark A, Akolkar B, Ziegler AG, Krischer JP; TEDDY study group.
+#' Biomarker discovery study design for type 1 diabetes in The
+#' Environmental Determinants of Diabetes in the Young (TEDDY) study.
+#' Diabetes Metab Res Rev. 2014 Jul;30(5):424-34. doi: 10.1002/dmrr.2510.
+#' PMID: 24339168; PMCID: PMC4058423.
+#' \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4058423/}
+#'
+#' @source The raw data can be downloaded from the Metabolomics workbench
+#' under study ID \strong{ST001386}:
+#' \url{https://www.metabolomicsworkbench.org/data/DRCCStudySummary.php?Mode=SetupRawDataDownload&StudyID=ST001386}
+"T1Dmeta"
+
+#' Example results for DNEA
+#'
+#' This data is an example DNEAobj object after performing DNEA. The
+#' experiment was performed using the \code{\link{TEDDY}} data with
+#' 4 reps of stability selection, no subsampling, and default parameters
+#' everywhere else. The seed was set to 417.
+#'
+#' @returns A \code{\link{DNEAobj}} object containing the results of a
+#' DNEA experiment
+#' @docType data
+#' @keywords datasets
+#' @name dnw
+#' @usage data("dnw")
+#' @format A DNEA results object after completing a DNEA experiment.
+#' @source The data is stored in the \code{\link{DNEA}} package and
+#' can be accessed by running data(dnw) in the console.
+"dnw"
+
+
