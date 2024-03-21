a64866fbc81c40cb55824cfeb28728426f874342
chrispatsalis <patsalis@umich.edu>
2024-03-21 17:15:49 -0400

    fixed bug in test related to data check:
    An error in the labeling of the columns in the simulated data matrix was causing a test failure.

--- tests/testthat/test-utilities-internals.R
+++ tests/testthat/test-utilities-internals.R
@@ -2,6 +2,7 @@ test_that("split_by_condition fails", {
 
   dat <- cbind(matrix(1, nrow = 5, ncol = 10), matrix(2, nrow = 5, ncol = 10))
   colnames(dat) <- paste0("sample", 1:ncol(dat))
+  rownames(dat) <- paste0("metab", 1:nrow(dat))
 
   sample_conditions <- c("control", "case", "case", "control", "case")
   names(sample_conditions) <- paste0("test", 1:length(sample_conditions))
@@ -19,6 +20,6 @@ test_that("split_by_condition fails", {
   names(sample_conditions) <- paste0("sample", 1:length(sample_conditions))
   expect_equal(split_by_condition(dat, condition_levels = c("control", "case"),
                                   condition_by_sample = sample_conditions),
-               list(control = matrix(1, nrow = 5, ncol = 10, dimnames = list(c(), paste0("sample", 1:10))),
-                    case = matrix(2, nrow = 5, ncol = 10, dimnames = list(c(), paste0("sample", 11:20)))))
+               list(control = matrix(1, nrow = 5, ncol = 10, dimnames = list(paste0("metab", 1:5), paste0("sample", 1:10))),
+                    case = matrix(2, nrow = 5, ncol = 10, dimnames = list(paste0("metab", 1:5), paste0("sample", 11:20)))))
 })
